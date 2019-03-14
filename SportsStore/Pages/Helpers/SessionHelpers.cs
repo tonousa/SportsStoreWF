e8f04f547c027a8170c8f1744254fdae4041fc80
tonousa <tonousa@hotmail.com>
2019-03-14 09:09:31 -0400

    session start

---
+++ SportsStore/Pages/Helpers/SessionHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace SportsStore.Pages.Helpers
+{
+    public enum SessionKey
+    {
+        CART,
+        RETURN_URL
+    }
+
+    public static class SessionHelpers
+    {
+        public static void Set(HttpSessionState session, SessionKey key, object value)
+        {
+            session[Enum.GetName(key] = value;
+        }
+    }
+}
