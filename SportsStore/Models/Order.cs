9eaa749fe20e3185bec71a9e5687f6e39add9277
tonousa <tonousa@hotmail.com>
2019-03-20 12:16:43 -0400

    198

--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportsStore.Models
 {
     public class Order
     {
         public int OrderId { get; set; }
+
+        [Required(ErrorMessage ="enter name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="enter first address line")]
         public string Line1 { get; set; }
         public string Line2 { get; set; }
         public string Line3 { get; set; }
+
+        [Required(ErrorMessage ="enter city")]
         public string City { get; set; }
+
+        [Required(ErrorMessage ="enter state")]
         public string State { get; set; }
         public bool GiftWrap { get; set; }
         public bool Dispatched { get; set; }
