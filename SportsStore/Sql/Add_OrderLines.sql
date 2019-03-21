46721acf738729c115a4e549b167a4659849f546
tonousa <tonousa@hotmail.com>
2019-03-21 16:28:20 -0400

    finish orders

---
+++ SportsStore/Sql/Add_OrderLines.sql
@@ -0,0 +1,18 @@
+﻿INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (1, 7, 5);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (2, 8, 5);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (1, 9, 5);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (2, 10, 5);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (1, 3, 6);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (2, 4, 6);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (1, 6, 6);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (2, 1, 7);
+INSERT INTO OrderLines (Quantity, Product_ProductID, Order_OrderId) 
+VALUES (1, 2, 7);
